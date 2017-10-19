ba3dc51df8f646b36b3a121b4bc1caa18fbd080a
dllo <dllo@weishibodeMac-mini.local>
2017-10-19 19:56:51 +0800

    修改home1

--- home1.js
+++ home1.js
@@ -1,11 +1,33 @@
 //克隆函数
 
-Array.prototype.clone=function () {
-                var o=[];
-              this.forEach(function (value,index) {
-                    o[index]=value;
-              })
-               console.log(o);
+// Array.prototype.clone=function () {
+//                 var o=[];
+//               this.forEach(function (value,index) {
+//                     o[index]=value;
+//               })
+//     console.log(o);
+// }
+var Arr=[1,2,3,4,56,87];
+//arr.clone();
+
+
+Array.prototype.myClone=function (callback,thisArg) {
+    if(typeof callback!=='function'){
+        throw new TypeError(callback+'is not a function');
+    }
+    for(var i=0;i<this.length;i++){
+        //判断值是否有效
+        if(Object.prototype.hasOwnProperty.call(this,i)){
+            callback(this[i],i,this)
+        }
+
+    }
 }
-var arr=[1,2,3,4,56,87];
-arr.clone();
+Arr.myClone(function (value,index,arr) {
+    var o=[];
+    arr.forEach(function (value,index) {
+        o[index]=value;
+    })
+    console.log(o)
+})
+;
